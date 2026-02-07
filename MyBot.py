c8bf29b538c41f129f98a24b5f307ca009e388c7
Priyansh Kohadkar <94213144+PriyanshKohadkar@users.noreply.github.com>
2026-02-07 21:07:27 +0530

    donotdisturb status for bot fixed

--- MyBot.py
+++ MyBot.py
@@ -53,7 +53,7 @@ def delete_encouragement(index):
 
 @client.event
 async def on_ready():
-  await client.change_presence(status=discord.Status.dnd, activity=discord.Game("Pressure of a scorer"))
+  await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game("Pressure of a scorer"))
   print('we have logged in as {0.user}'.format(client))
 
 
@@ -149,6 +149,7 @@ async def on_message(message):
 
 #keep_alive()
 client.run(TOKEN)   
+
 
 
 
