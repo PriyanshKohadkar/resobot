f3334ae031d80e82d3ab49d6cb6d801c713afb98
Priyansh Kohadkar <94213144+PriyanshKohadkar@users.noreply.github.com>
2026-02-09 01:21:16 +0530

    added port (Taken from render environment) for flask keepalive
    Added environment variable for render to fix binding port error

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,17 @@
+import os
 from flask import Flask
 from threading import Thread
 
 app = Flask('')
-
+port = int(os.environ.get('PORT', 5000))
 @app.route('/')
 def home():
     return "Hello. I am alive!"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  app.run(host='0.0.0.0',port=port)
 
 def keep_alive():
     t = Thread(target=run)
+
     t.start()
