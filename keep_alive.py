bf593d944fff809d37e79ae69d936f56af976a02
Priyansh Kohadkar <94213144+PriyanshKohadkar@users.noreply.github.com>
2026-02-09 02:09:07 +0530

    port environment keep_Alive

--- keep_alive.py
+++ keep_alive.py
@@ -3,16 +3,17 @@ from flask import Flask
 from threading import Thread
 
 app = Flask('')
-#port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT')
 @app.route('/')
 def home():
     return "Hello. I am alive!"
 
-#def run():
-  #app.run(host='0.0.0.0',port=port)
+def run():
+  app.run(host='0.0.0.0',port=port)
 
 def keep_alive():
     t = Thread(target=run)
 
     t.start()
+
 
